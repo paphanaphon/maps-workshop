9315d962e3c2ea93d65e963c98388861b1549519
paphanaphon <59946176+paphanaphon@users.noreply.github.com>
2020-01-16 21:54:24 +0700

    Update my_map1.py

--- my_map1.py
+++ my_map1.py
@@ -5,8 +5,7 @@ import folium
   
 # Here we pass coordinates of Gfg  
 # and starting Zoom level = 12 
-my_map1=folium.Map(location=[13.845760,100.5701],
-                                              zoom_start=20)
+my_map1=folium.Map(location=[13.845760,100.5701], zoom_start=20)
  # save method of Map object will create a map 
 my_map1.save(" my_map1.html " ) 
                                              
