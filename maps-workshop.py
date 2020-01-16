97b8f4f8bdd75ae038c57c6a4ed5ae527febfd85
paphanaphon <59946176+paphanaphon@users.noreply.github.com>
2020-01-16 21:43:47 +0700

    Add files via upload

---
+++ maps-workshop.py
@@ -0,0 +1,12 @@
+# import folium package 
+import folium 
+  
+# Map method of folium return Map object 
+  
+# Here we pass coordinates of Gfg  
+# and starting Zoom level = 12 
+my_map1=folium.Map(location=[13.845760,100.5701],
+                                              zoom_start=20)
+ # save method of Map object will create a map 
+my_map1.save(" my_map1.html " ) 
+                                             
