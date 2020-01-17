9033950647a27fada82fd72c20376b22131e77a7
paphanaphon <59946176+paphanaphon@users.noreply.github.com>
2020-01-17 22:44:57 +0700

    Update my-map3.py

--- my-map3.py
+++ my-map3.py
@@ -1,12 +1,9 @@
 # import folium package 
 import folium 
   
-my_map3 = folium.Map(location = [ 13.846128,100.56928 ], 
-                                        zoom_start = 20) 
+my_map3 = folium.Map(location = [ 13.845577,100.56959 ], zoom_start = 20) 
   
 # Pass a string in popup parameter 
-folium.Marker([13.846621,100.569225], 
-               popup = 'ENG KU .').add_to(my_map3) 
-  
-  
+folium.Marker([13.845577,100.56959],  popup = 'ENG KU .').add_to(my_map3) 
+
 my_map3.save(" my_map3.html ") 
